17d7c4a8e6001aa4092d04096a37ae898af9e197
George Yegon <georgeyegon67@gmail.com>
2024-12-08 22:54:04 +0300

    initial commit

--- lib/department.py
+++ lib/department.py
@@ -2,7 +2,6 @@ from __init__ import CURSOR, CONN
 
 
 class Department:
-
     def __init__(self, name, location, id=None):
         self.id = id
         self.name = name
@@ -10,3 +9,52 @@ class Department:
 
     def __repr__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
+
+    @classmethod
+    def create_table(cls):
+        """Creates the 'departments' table if it doesn't exist."""
+        sql = """
+        CREATE TABLE IF NOT EXISTS departments (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            location TEXT
+        )
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        """Drops the 'departments' table if it exists."""
+        sql = "DROP TABLE IF EXISTS departments"
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self):
+        """Saves a Department instance to the database and assigns it an id."""
+        if self.id is None:
+            sql = "INSERT INTO departments (name, location) VALUES (?, ?)"
+            CURSOR.execute(sql, (self.name, self.location))
+            CONN.commit()
+            self.id = CURSOR.lastrowid
+        else:
+            self.update()
+
+    @classmethod
+    def create(cls, name, location):
+        """Creates a new department record and returns a Department instance."""
+        department = cls(name, location)
+        department.save()
+        return department
+
+    def update(self):
+        """Updates the database record to match the instance's attributes."""
+        sql = "UPDATE departments SET name = ?, location = ? WHERE id = ?"
+        CURSOR.execute(sql, (self.name, self.location, self.id))
+        CONN.commit()
+
+    def delete(self):
+        """Deletes the instance's corresponding record from the database."""
+        sql = "DELETE FROM departments WHERE id = ?"
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
